95140061b6863aed63af594285569a6afbaec99b
Huy <changnhung28@gmail.com>
2026-03-18 18:06:59 +0700

    show home page

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('go/<int:id>/', views.go_link),
-    path('register/', views.register_redirect),
-    path('login/', views.login_redirect),
-    path('register_code/', views.register_code),
-    path('register_tele/', views.register_tele),
+    path('', views.home, name='home'),
+    path('go/<int:id>/', views.go_link, name='go_link'),
+
+    path('register/', views.register_redirect, name='register'),
+    path('login/', views.login_redirect, name='login'),
+    path('code/', views.register_code, name='code'),
+    path('tele/', views.register_tele, name='tele'),
 ]
